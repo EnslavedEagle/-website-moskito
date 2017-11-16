0d949639e888acdac086c7e63f25ebf15f1e0e2a
Patryk Bernasiewicz <kontakt@patrykb.pl>
2017-11-16 17:51:17 +0100

    Home page

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,37 @@
-((() => {
+(() => {
   'use strict';
   
   const root = document.documentElement;
   const body = document.body;
   const hamburger = document.getElementById('nav__hamburger');
-  const nav = document.getElementById('nav')
+  const nav = document.getElementById('main-navigation')
+  const navLinks = document.querySelectorAll('.nav a');
 
-  hamburger.addEventListener('click', () => {
+  let scrollTimeout = null;
+  function stickyNav() {
+    scrollTimeout = setTimeout(() => {
+      clearTimeout(scrollTimeout);
+      if (root.scrollTop > 0) {
+        nav.classList.remove('nav--sticky');
+      } else {
+        nav.classList.add('nav--sticky');
+      }
+    }, 100);
+  }
+
+  function toggleNavigation() {
     root.classList.toggle('locked');
     nav.classList.toggle('nav--open');
-  });
+  }
 
-  body.addEventListener('scroll', () => {
-    console.log('scroll!');
-  });
-})());
+  hamburger.addEventListener('click', toggleNavigation, false);
+
+  document.addEventListener('scroll', stickyNav, false);
+  stickyNav();
+
+  for (let i = 0, len = navLinks.length; i < len; i++) {
+    (function(i) {
+      navLinks[i].addEventListener('click', toggleNavigation, false);
+    }(i));
+  }
+})();
